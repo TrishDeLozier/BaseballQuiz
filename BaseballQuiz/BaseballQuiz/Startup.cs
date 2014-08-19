f0d456c1ca134524458d135eb46fdd5029ffb289
TrishDeLozier <trishdelozier@gmail.com>
2014-08-19 19:41:01 -0400

    Initial Commit/Add Models
    Initial Commit/Add Models

---
+++ BaseballQuiz/BaseballQuiz/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BaseballQuiz.Startup))]
+namespace BaseballQuiz
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
